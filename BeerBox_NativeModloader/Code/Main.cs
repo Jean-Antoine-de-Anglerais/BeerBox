1db44cc7a0271fb75222ec06daac7011a33f91a9
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-10-04 18:24:23 +0300

    Project refactoring

--- BeerBox_NativeModloader/Code/Main.cs
+++ BeerBox_NativeModloader/Code/Main.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
@@ -13,10 +14,76 @@ namespace BeerBox_NativeModloader
         {
             if (global::Config.gameLoaded && !_initialized)
             {
-                harmony.Patch(AccessTools.Method(typeof(WorldLawElement), nameof(WorldLawElement.click)),
-                transpiler: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.click_Transpiler))));
+                harmony.Patch(AccessTools.Method(typeof(BuildingLibrary), nameof(BuildingLibrary.addResource)),
+                    prefix: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.addResource_Prefix))));
+
+                harmony.Patch(AccessTools.Method(typeof(ItemAsset), nameof(ItemAsset.setCost)),
+                    prefix: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.setCost_Prefix))));
+
+                harmony.Patch(AccessTools.Method(typeof(CityStorage), nameof(CityStorage.change)),
+                    prefix: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.change_Prefix))));
+
+                harmony.Patch(AccessTools.Method(typeof(CityStorage), nameof(CityStorage.addNew)),
+                    prefix: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.addNew_Prefix))));
+
+                harmony.Patch(AccessTools.Method(typeof(TooltipLibrary), nameof(TooltipLibrary.showResource)),
+                    transpiler: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.showResource_Transpiler))));
+
+                harmony.Patch(AccessTools.Method(typeof(ActorBagExtensions), nameof(ActorBagExtensions.add)),
+                    prefix: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.add_Prefix))));
+
+                var beer = AssetManager.resources.get(SR.ale);
+                beer.maximum = 9999;
+                beerLocalization("ar", beer.id, "البيرة");
+                beerLocalization("boat", beer.id, "Beeroat");
+                beerLocalization("br", beer.id, "Cerveja");
+                beerLocalization("by", beer.id, "Піва");
+                beerLocalization("ch", beer.id, "啤酒");
+                beerLocalization("cs", beer.id, "Pivo");
+                beerLocalization("cz", beer.id, "啤酒");
+                beerLocalization("da", beer.id, "Øl");
+                beerLocalization("de", beer.id, "Bier");
+                beerLocalization("en", beer.id, "Beer");
+                beerLocalization("es", beer.id, "Cerveza");
+                beerLocalization("fa", beer.id, "آبجو");
+                beerLocalization("fn", beer.id, "Olut");
+                beerLocalization("fr", beer.id, "Bière");
+                beerLocalization("gr", beer.id, "Μπύρα");
+                beerLocalization("he", beer.id, "בירה");
+                beerLocalization("hi", beer.id, "बीयर");
+                beerLocalization("hr", beer.id, "Pivo");
+                beerLocalization("hu", beer.id, "Sör");
+                beerLocalization("id", beer.id, "Bir");
+                beerLocalization("it", beer.id, "Birra");
+                beerLocalization("ja", beer.id, "ビール");
+                beerLocalization("ka", beer.id, "ლუდი");
+                beerLocalization("ko", beer.id, "맥주");
+                beerLocalization("lb", beer.id, "Béier");
+                beerLocalization("nl", beer.id, "Bier");
+                beerLocalization("no", beer.id, "Øl");
+                beerLocalization("ph", beer.id, "Serbesa");
+                beerLocalization("pl", beer.id, "Piwo");
+                beerLocalization("pt", beer.id, "Cerveja");
+                beerLocalization("ro", beer.id, "Bere");
+                beerLocalization("ru", beer.id, "Пиво");
+                beerLocalization("sk", beer.id, "Pivo");
+                beerLocalization("sv", beer.id, "Öl");
+                beerLocalization("th", beer.id, "เบียร์");
+                beerLocalization("tr", beer.id, "Bira");
+                beerLocalization("ua", beer.id, "Пиво");
+                beerLocalization("vn", beer.id, "Bia");
 
                 _initialized = true;
+            }
+        }
+
+        public static void beerLocalization(string planguage, string id, string name)
+        {
+            if (planguage == LocalizedTextManager.instance.language)
+            {
+                Dictionary<string, string> localizedText = LocalizedTextManager.instance.localizedText;
+                localizedText.Remove("ale");
+                localizedText.Add(id, name);
             }
         }
     }
--- BeerBox_NativeModloader/Code/Patches.cs
+++ BeerBox_NativeModloader/Code/Patches.cs
@@ -9,37 +9,59 @@ namespace BeerBox_NativeModloader
 {
     public static class Patches
     {
-        public static IEnumerable<CodeInstruction> click_Transpiler(IEnumerable<CodeInstruction> instructions)
+        public static void addResource_Prefix(BuildingLibrary __instance, string pID, int pAmount, bool pNewList = false)
+        {
+            pID = SR.ale;
+        }
+
+        public static void setCost_Prefix(ItemAsset __instance, int pGoldCost, string pResourceID_1 = "none", int pCostResource_1 = 0, string pResourceID_2 = "none", int pCostResource_2 = 0)
+        {
+            if (pResourceID_1 != "none")
+            {
+                pResourceID_1 = SR.ale;
+            }
+
+            if (pResourceID_2 != "none")
+            {
+                pResourceID_2 = SR.ale;
+            }
+        }
+
+        public static void change_Prefix(CityStorage __instance, ref int __result, string pRes, int pAmount = 1)
+        {
+            pRes = SR.ale;
+        }
+
+        public static void addNew_Prefix(CityStorage __instance, ref int __result, string pResID, int pAmount)
+        {
+            pResID = SR.ale;
+        }
+
+        public static void add_Prefix(this ActorBag pBag, string pID, int pAmount)
+        {
+            pID = SR.ale;
+        }
+
+        public static IEnumerable<CodeInstruction> showResource_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             var codes = new List<CodeInstruction>(instructions);
-             
-            int start = -1; 
-
-            int end = -1;
 
             for (int i = 0; i < codes.Count; i++)
             {
-                if (codes[i].Is(OpCodes.Ldstr, "world_law_diplomacy"))
+                if (codes[i].Is(OpCodes.Ldsfld, AccessTools.Field(typeof(SR), nameof(SR.gold))))
                 {
-                    start = i - 2;
+                    codes[i].operand = AccessTools.Field(typeof(SR), nameof(SR.ale));
 
                     break;
                 }
             }
-
-            for (int i = 0; i < codes.Count; i++)
-            {
-                if (codes[i].Is(OpCodes.Callvirt, AccessTools.Method(typeof(WarManager), nameof(WarManager.stopAllWars))))
-                {
-                    end = i;
-
-                    break;
-                }
-            }
-
-            codes.RemoveRange(start, end - start + 1);
 
             return codes.AsEnumerable();
+        }
+
+        public static void get_Prefix(CityStorage __instance, ref int __result, string pRes)
+        {
+            pRes = SR.ale;
         }
     }
 }
