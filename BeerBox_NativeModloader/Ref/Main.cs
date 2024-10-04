e7a19497c84826ae9a7ab6c6fdf78751728ba877
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-10-04 18:18:03 +0300

    Добавьте файлы проекта.

---
+++ BeerBox_NativeModloader/Ref/Main.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static Config;
+using ReflectionUtility;
+using System.Reflection;
+using UnityEngine.Tilemaps;
+using System.IO;
+using NCMS;
+
+namespace BeerBoxNCMS
+{
+    [ModEntry]
+    class Main : MonoBehaviour
+    {
+        void Awake()
+        {
+            BeerBoxClass.init();
+        }
+    }
+}
